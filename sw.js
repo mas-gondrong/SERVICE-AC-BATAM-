67e818a741288cc377bcc19e58c6b1e7da5add23
mas-gondrong <ummayah181@gmail.com>
2026-09-24 14:08:59 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'ac-service-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/manifest.json'
+];
+
+// Install Service Worker dan simpan aset ke Cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ambil aset dari cache jika offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
